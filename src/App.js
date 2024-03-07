c750f395fdc98bf25cc5748f62932365cd9ab99d
anuhyanallapati <anuhyanallapati@gmail.com>
2024-03-07 21:07:22 +0530

    fixed error-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,95 @@
 
 
+// // App.js
+// import React, { useRef } from 'react';
+// import './styles.css';
+// import Whitebox from './components/Whitebox';
+// import NavigationBar from './components/NavigationBar'; // Import the original navigation bar
+// import Bluebox from './components/Bluebox';
+// import NewNavigationBar from './components/NewNavigationBar';
+// import Overview from './components/Overview';
+// import Fundamentals from './components/Fundamentals';
+// import NewsInsights from './components/NewsInsights';
+// import Sentiments from './components/Sentiments';
+// import Team from './components/Team';
+// import Technicals from './components/Technicals';
+// import Tokenomics from './components/Tokenomics';
+
+// const App = () => {
+//   const overviewRef = useRef(null);
+//   const fundamentalsRef = useRef(null);
+//   const newsInsightsRef = useRef(null);
+//   const sentimentsRef = useRef(null);
+//   const teamRef = useRef(null);
+//   const technicalsRef = useRef(null);
+//   const tokenomicsRef = useRef(null);
+
+//   const scrollToSection = (section) => {
+//     switch (section) {
+//       case 'overview':
+//         overviewRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'fundamentals':
+//         fundamentalsRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'news-insights':
+//         newsInsightsRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'sentiments':
+//         sentimentsRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'team':
+//         teamRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'technicals':
+//         technicalsRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       case 'tokenomics':
+//         tokenomicsRef.current.scrollIntoView({ behavior: 'smooth' });
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   return (
+//     <div className="app">
+//       <NavigationBar />
+//       <div className="container">
+//         <Whitebox />
+//         <Bluebox />
+//       </div>
+//       {/* New navigation bar */}
+//       <NewNavigationBar scrollToSection={scrollToSection} />
+//       {/* Section components */}
+//       <div ref={overviewRef}>
+//         <Overview />
+//       </div>
+//       <div ref={fundamentalsRef}>
+//         <Fundamentals />
+//       </div>
+//       {/* <div ref={newsInsightsRef}>
+//         <NewsInsights />
+//       </div>
+//       <div ref={sentimentsRef}>
+//         <Sentiments />
+//       </div> */}
+//       <div ref={teamRef}>
+//         <Tokenomics />
+//       </div>
+//       {/* <div ref={technicalsRef}>
+//         <Technicals />
+//       </div> */}
+//       <div ref={tokenomicsRef}>
+//         <Team />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 // App.js
 import React, { useRef } from 'react';
 import './styles.css';
@@ -9,19 +99,13 @@ import Bluebox from './components/Bluebox';
 import NewNavigationBar from './components/NewNavigationBar';
 import Overview from './components/Overview';
 import Fundamentals from './components/Fundamentals';
-import NewsInsights from './components/NewsInsights';
-import Sentiments from './components/Sentiments';
 import Team from './components/Team';
-import Technicals from './components/Technicals';
 import Tokenomics from './components/Tokenomics';
 
 const App = () => {
   const overviewRef = useRef(null);
   const fundamentalsRef = useRef(null);
-  const newsInsightsRef = useRef(null);
-  const sentimentsRef = useRef(null);
   const teamRef = useRef(null);
-  const technicalsRef = useRef(null);
   const tokenomicsRef = useRef(null);
 
   const scrollToSection = (section) => {
@@ -32,17 +116,8 @@ const App = () => {
       case 'fundamentals':
         fundamentalsRef.current.scrollIntoView({ behavior: 'smooth' });
         break;
-      case 'news-insights':
-        newsInsightsRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case 'sentiments':
-        sentimentsRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
       case 'team':
         teamRef.current.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case 'technicals':
-        technicalsRef.current.scrollIntoView({ behavior: 'smooth' });
         break;
       case 'tokenomics':
         tokenomicsRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -68,20 +143,11 @@ const App = () => {
       <div ref={fundamentalsRef}>
         <Fundamentals />
       </div>
-      {/* <div ref={newsInsightsRef}>
-        <NewsInsights />
-      </div>
-      <div ref={sentimentsRef}>
-        <Sentiments />
-      </div> */}
       <div ref={teamRef}>
-        <Tokenomics />
-      </div>
-      {/* <div ref={technicalsRef}>
-        <Technicals />
-      </div> */}
-      <div ref={tokenomicsRef}>
         <Team />
+      </div>
+      <div ref={tokenomicsRef}>
+        <Tokenomics />
       </div>
     </div>
   );
--- src/components/Bluebox.js
+++ src/components/Bluebox.js
@@ -16,20 +16,38 @@
 // }
 
 // export default Bluebox;
+
+// Bluebox.js
 import React from 'react';
 import imageSrc from '../images/bluepic.png'; // Assuming this is your image source
 
-class Bluebox extends React.Component {
-  render() {
-    return (
-      <div className="blue-box">
-        <h2>Get Started with KoinX for FREE</h2>
-        <p>With our range of features that you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</p>
-        <img src={imageSrc} alt="Description of the image" style={{ display: 'block', margin: '0 auto', width: '50%' }} />
-        <p>Get Started for FREE</p>
-      </div>
-    );
-  }
+const Bluebox = () => {
+  return (
+    <div className="blue-box">
+      <h2>Get Started with KoinX for FREE</h2>
+      <p>With our range of features that you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</p>
+      <img src={imageSrc} alt="KoinX Logo" style={{ display: 'block', margin: '0 auto', width: '50%' }} />
+      <p>Get Started for FREE</p>
+    </div>
+  );
 }
 
 export default Bluebox;
+
+// import React from 'react';
+// import imageSrc from '../images/bluepic.png'; // Assuming this is your image source
+
+// class Bluebox extends React.Component {
+//   render() {
+//     return (
+//       <div className="blue-box">
+//         <h2>Get Started with KoinX for FREE</h2>
+//         <p>With our range of features that you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</p>
+//         <img src={imageSrc} alt="Description of the image" style={{ display: 'block', margin: '0 auto', width: '50%' }} />
+//         <p>Get Started for FREE</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Bluebox;
